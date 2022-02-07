d1680a1924acbb11cc163470557d715c0abc47db
repaschall <repaschall@gmail.com>
2022-02-07 15:23:34 -0500

    Modified port used by mock api to match port used by actual api to minimize changes needed to switch targets.

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -25,7 +25,7 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      "process.env.API_URL": JSON.stringify("http://localhost:3001")
+      "process.env.API_URL": JSON.stringify("http://localhost:44396")
     }),
     new HtmlWebpackPlugin({
       template: "src/index.html",
